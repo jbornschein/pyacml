6f7ababfdf4223f7e62adbc580acd7a32408ce3b
Joerg Bornschein <bornschein@fias.uni-frankfurt.de>
2010-09-19 21:28:53 +0200

    Add some acml_mv signatures

--- pyacml/vm.pyx
+++ pyacml/vm.pyx
@@ -4,7 +4,12 @@ cimport numpy as np
 
 
 cdef extern from "acml_mv.h":
+    int vrda_sin(int n, double a[], double y[])
+    int vrda_cos(int n, double a[], double y[])
     int vrda_exp(int n, double a[], double y[])
+    int vrda_log(int n, double a[], double y[])
+    int vrda_log10(int n, double a[], double y[])
+    int vrda_log2(int n, double a[], double y[])
 
 
 def vdexp(np.ndarray[np.float64_t] arr):
