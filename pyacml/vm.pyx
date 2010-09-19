d86dddc93f4cd61a37232e5f31c8f0b529e07dc4
Joerg Bornschein <bornschein@fias.uni-frankfurt.de>
2010-09-19 21:47:50 +0200

    Add some more vector functions

--- pyacml/vm.pyx
+++ pyacml/vm.pyx
@@ -12,10 +12,48 @@ cdef extern from "acml_mv.h":
     int vrda_log2(int n, double a[], double y[])
 
 
-def vdexp(np.ndarray[np.float64_t] arr):
+##############################################################################
+# Function wrappers
+
+def sin(np.ndarray[np.float64_t] arr):
+    cdef np.ndarray[np.float64_t] out = np.empty_like(arr)
+    cdef int n = arr.shape[0]
+
+    vrda_sin(n, <double*>arr.data, <double*>out.data)
+    return out
+
+def cos(np.ndarray[np.float64_t] arr):
+    cdef np.ndarray[np.float64_t] out = np.empty_like(arr)
+    cdef int n = arr.shape[0]
+
+    vrda_cos(n, <double*>arr.data, <double*>out.data)
+    return out
+
+def exp(np.ndarray[np.float64_t] arr):
     cdef np.ndarray[np.float64_t] out = np.empty_like(arr)
     cdef int n = arr.shape[0]
 
     vrda_exp(n, <double*>arr.data, <double*>out.data)
+    return out
+
+def log(np.ndarray[np.float64_t] arr):
+    cdef np.ndarray[np.float64_t] out = np.empty_like(arr)
+    cdef int n = arr.shape[0]
+
+    vrda_log(n, <double*>arr.data, <double*>out.data)
+    return out
+
+def log10(np.ndarray[np.float64_t] arr):
+    cdef np.ndarray[np.float64_t] out = np.empty_like(arr)
+    cdef int n = arr.shape[0]
+
+    vrda_log10(n, <double*>arr.data, <double*>out.data)
+    return out
+
+def log2(np.ndarray[np.float64_t] arr):
+    cdef np.ndarray[np.float64_t] out = np.empty_like(arr)
+    cdef int n = arr.shape[0]
+
+    vrda_log2(n, <double*>arr.data, <double*>out.data)
     return out
 
